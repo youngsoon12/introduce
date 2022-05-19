88380b095ae9170588c21ce74939429695562269
youngsoon12 <20151643@vision.hoseo.edu>
2022-05-20 06:37:26 +0900

    커서 작업중

---
+++ dumy/script.js
@@ -0,0 +1,13 @@
+const content = "35안녕하세요 개발자를 꿈꾸는 김영수 입니다.";
+const text = document.querySelector(".text");
+let index = 0;
+
+function typing() {
+  text.textContent += content[index++];
+  if (index > content.length) {
+    text.textContent = "";
+    index = 0;
+  }
+}
+
+setInterval(typing, 300);
