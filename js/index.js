670fd59bd12c5085f257c82ae1686bd40647d528
youngsoon12 <20151643@vision.hoseo.edu>
2022-05-22 19:14:08 +0900

    1.0ver

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-const content = "안녕하세요 개발자를 꿈꾸는 김영수 입니다";
+let content = "안녕하세요 개발자를 꿈꾸는 김영수 입니다";
 const text = document.querySelector(".text");
 let index = 0;
+let count = 0;
 
 function typing() {
   text.textContent += content[index++];
@@ -8,6 +9,43 @@ function typing() {
     text.textContent = "";
     index = 0;
   }
+  count++;
 }
 
-setInterval(typing, 200);
+function insta() {
+  content = "loading...";
+  if ((text.textContent = "loading...")) {
+    index = 22;
+    content = "인스타그램으로 이동합니다...";
+    setInterval(typing, 1500);
+  }
+}
+
+function insta() {
+  clearInterval(typing);
+  content = "loading...";
+  if ((text.textContent = "loading...")) {
+    index = 22;
+    content = "인스타그램으로 이동합니다...";
+  }
+}
+
+function github() {
+  clearInterval(typing);
+  content = "loading...";
+  if ((text.textContent = "loading...")) {
+    index = 22;
+    content = "깃허브로 이동합니다...";
+  }
+}
+
+function out() {
+  clearInterval(typing);
+  content = "loading...";
+  if ((text.textContent = "loading...")) {
+    index = 22;
+    content = "안녕하세요 개발자를 꿈꾸는 김영수 입니다";
+  }
+}
+
+setInterval(typing, 100);
